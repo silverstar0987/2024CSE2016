41c2c055210b002cfb454fdc8871b5048fb3fc2a
eunseong <silverstar103176@gmail.com>
2024-10-06 18:55:18 +0900

    View.getTemperture 완성

--- lectures/src/lecture03/temperature/View.java
+++ lectures/src/lecture03/temperature/View.java
@@ -1,11 +1,17 @@
 package lecture03.temperature;
 
+import javax.swing.*;
+
 public class View {
     double getTemperature() {
-        return 0.0;
+        String input = JOptionPane.showInputDialog(
+                null,"섭씨 온도를 입력하세요.");
+        int c = Integer.parseInt(input);
+        return c;
     }
 
     void showTemperature(double c, double f) {
+
         System.out.println("섭씨 " + c + "도를 화씨로 바꾸면 " + f + "도입니다.");
     }
 }
